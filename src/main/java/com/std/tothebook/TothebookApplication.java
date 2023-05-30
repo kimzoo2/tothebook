b6950d449307ca5a654197bea43e4856aaa34828
kimzoo2 <kimzoo1207@gmail.com>
2023-05-30 21:13:41 +0900

    Chore : 프로젝트 DB 세팅
    - properties 파일 제거
    - yml 파일 추가
    - Application exclude 옵션 제거

--- src/main/java/com/std/tothebook/TothebookApplication.java
+++ src/main/java/com/std/tothebook/TothebookApplication.java
@@ -2,9 +2,8 @@ package com.std.tothebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication()
 public class TothebookApplication {
 
 	public static void main(String[] args) {
