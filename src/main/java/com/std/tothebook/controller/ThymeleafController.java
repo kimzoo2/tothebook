7f739a35585503f38dc7633699bc9c650140ee3e
kimzoo2 <kimzoo1207@gmail.com>
2023-06-11 21:29:04 +0900

    Design : 예시용 화면 공통 영역 구성
    - 화면 헤더/네비게이터바/푸터 영역 추가
    - 공통 영역 호출하는 index 추가
    - index 화면 호출하는 ThymeleafController 추가

---
+++ src/main/java/com/std/tothebook/controller/ThymeleafController.java
@@ -0,0 +1,14 @@
+package com.std.tothebook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ThymeleafController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
