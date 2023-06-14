7e03cf8c1cfd56ce28966ac639c9f6b458453471
seulhui <lsh9412@naver.com>
2023-06-14 21:48:29 +0900

    Temp: 컨트롤러 내 view 폴더 확인 및 메인 조회용 컨트롤러 임시 추가

---
+++ src/main/java/com/std/tothebook/api/controller/view/MainViewController.java
@@ -0,0 +1,9 @@
+package com.std.tothebook.api.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main")
+public class MainViewController {
+}
