7de612988b8ee390f6bd94a4744ced7d8c062169
seulhui <lsh9412@naver.com>
2023-06-18 21:00:23 +0900

    Feat: 에러 핸들링에 에러 로그 추가

--- src/main/java/com/std/tothebook/exception/GlobalExceptionHandler.java
+++ src/main/java/com/std/tothebook/exception/GlobalExceptionHandler.java
@@ -1,19 +1,24 @@
 package com.std.tothebook.exception;
 
 import com.std.tothebook.exception.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ExpectedException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(ExpectedException e) {
-        // TODO log
-        System.out.println(e.toString());
+        this.log(e);
         return StringUtils.hasText(e.getMessage()) ? ErrorResponse.errorWithMessage(e) : ErrorResponse.error(e);
+    }
+
+    private void log(ExpectedException e) {
+        log.error("{} : {}", e.toString(), e.getErrorCode() == null ? e.getMessage() : e.getErrorCode().getMessage());
     }
 }
