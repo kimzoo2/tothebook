558a2530034a85a803a91f40fd76d6b65be43ab5
seulhui <lsh9412@naver.com>
2023-12-20 21:28:29 +0900

    Feat: mail vo 수정
    - 메일 내용 세팅 추가
    - 메일 세팅 방식 변경

--- src/main/java/com/std/tothebook/vo/Mail.java
+++ src/main/java/com/std/tothebook/vo/Mail.java
@@ -22,18 +22,17 @@ public class Mail {
         this.to = to;
         this.certificationType = certificationType;
 
-        setMail();
+        setSubject();
+        setContent();
     }
 
-    // 제목, 내용 세팅
-    private void setMail() {
+    // 제목 세팅
+    private void setSubject() {
         if (certificationType.equals(CertificationType.SIGN_UP)) {
             this.subject = "[북쪽으로] 회원 가입 인증 번호 안내";
         } else if (certificationType.equals(CertificationType.FIND_PASSWORD)) {
             this.subject = "[북쪽으로] 비밀번호 찾기 인증 번호 안내";
         }
-
-        this.text = getEmailText();
     }
 
     // 인증번호 replace
@@ -41,9 +40,10 @@ public class Mail {
         this.text = this.text.replace("{certificationNumber}", certificationNumber);
     }
 
-    // 인증번호 전송 메일 본문 생성
-    private String getEmailText() {
-        return """
+    // 본문 세팅
+    private void setContent() {
+        if (this.certificationType.equals(CertificationType.SIGN_UP)) {
+            this.text = """
                 <div style='margin:2em;'>
                 <h1>북쪽으로</h1>
                 <br>
@@ -65,5 +65,31 @@ public class Mail {
                 감사합니다.
                 </span>
                 </div>""";
+        } else if (this.certificationType.equals(CertificationType.FIND_PASSWORD)) {
+            this.text = """
+                <div style='margin:2em;'>
+                <h1>북쪽으로</h1>
+                <br>
+                <span style="font-family: 맑은 고딕;">
+                북쪽으로,
+                <strong>
+                <span style="color:darkgreen"> 비밀번호 수정을 위한 이메일 인증번호</span>
+                </strong>
+                입니다.
+                <br>
+                <br>
+                안녕하세요.<br>북쪽으로 입니다.<br><br>
+                아래의 인증 번호를 입력해주세요.<br>
+                인증 유효 시간은 5분입니다.<br>
+                <div style="border:1px solid darkgreen; margin:1em 0 0 0; padding: 1em;">
+                <p>인증번호: <strong>{certificationNumber}</strong></p>
+                </div>
+                <br>
+                감사합니다.
+                </span>
+                </div>""";
+        } else {
+            this.text = "";
+        }
     }
 }
